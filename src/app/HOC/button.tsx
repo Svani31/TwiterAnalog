2a50eb39eacc94cd1707507f7ebf612d1cd696a6
Svani31 <gembani0@gmail.com>
2024-01-05 05:18:26 +0400

    completing ux on landing page

---
+++ src/app/HOC/button.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface ButtonProps {
+    text:string,
+  
+    style:any
+}
+
+function Button({text,style}:ButtonProps) {
+  return (
+    <button className={style}>{text}</button>
+  )
+}
+
+export default Button
