3c13a4d2af9801de2ae2378f33d12de1b9295786
Svani31 <gembani0@gmail.com>
2024-03-23 21:08:05 +0400

    dfg

--- src/app/(page)/page.tsx
+++ src/app/(page)/page.tsx
@@ -1,7 +1,17 @@
 "use client";
+import { useEffect } from "react";
 import LandingPage from "../components/landing/page";
 
 export default function Home() {
+  useEffect(() => {
+    var s1 = document.createElement("script");
+    var s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/65fee91a1ec1082f04da6ea7/1hpm3vogl';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+  }, []);
+
   return (
     <div>
       <LandingPage />
