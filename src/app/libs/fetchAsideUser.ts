f93d73a6bdec99869876273668d3e5d1ef2e478c
Svani31 <gembani0@gmail.com>
2024-08-26 13:23:14 +0400

    clearing /

--- src/app/libs/fetchAsideUser.ts
+++ src/app/libs/fetchAsideUser.ts
@@ -3,7 +3,7 @@ import { useSession } from "next-auth/react"
 
 export const asideUser = async(myUserId:string) =>{
     try{
-            const respons = await fetch(`${process.env.NEXT_PUBLIC_API_KEY}/api/user`,{
+            const respons = await fetch(`api/user`,{
                 method:"GET",
                 headers:{
                     "Content-Type":"application/json"
