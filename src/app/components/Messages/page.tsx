5c87e981acecacc2470161d9380e3bc7a6ec3876
Svani31 <gembani0@gmail.com>
2024-10-10 23:52:38 +0400

    working on messanger

--- src/app/components/Messages/page.tsx
+++ src/app/components/Messages/page.tsx
@@ -1,22 +1,30 @@
 "use client"
 import { Users } from "@/app/libs/eachUser";
 import { useQuery } from "@tanstack/react-query";
-import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import Chat from "./chat";
 
 function Message() {
 
-const {isError,isLoading,data} = useQuery<RegisterTypes[]>({
-  queryKey:["Users"],
-  queryFn:Users,
-  refetchOnMount:false
-})
+  const [userId,setUserId] = useState<string | undefined>("")
+  
+  const {isError,isLoading,data} = useQuery<RegisterTypes[]>({
+    queryKey:["Users"],
+    queryFn:Users,
+    refetchOnMount:false
+  })
+  
+  const getUserIdHandler = (id:string | undefined) =>{
+      setUserId(id)
+  }
 
   return (
     <div className="flex justify-between p-3">
       <div className="flex flex-col">
         {data?.map((eachData)=>{
           return(
-            <div key={eachData.id} className="p-4 hover:bg-slate-500 rounded-2xl mt-8">
+            <div key={eachData.id} onClick={(e)=> getUserIdHandler(eachData.id)} className="p-4 hover:bg-slate-500 rounded-2xl mt-8">
           <div className="flex gap-3">
             <img
               src={eachData.image}
@@ -33,34 +41,7 @@ const {isError,isLoading,data} = useQuery<RegisterTypes[]>({
         })}
       </div>
 
-      <div className="w-[1600px]  bg-slate-500 p-2 rounded-2xl">
-        <div className="flex items-center gap-3">
-          <img
-            src=""
-            alt="Image"
-            className="w-12 h-12 rounded-full bg-red-500"
-          />
-          <h1 className="flex flex-col">Users Name</h1>
-        </div>
-        <div className="h-[800px] mt-7">
-          <div className="flex justify-between flex-col">
-            <div className=" w-fit">
-              <h1 className="bg-blue-600 p-3 rounded-3xl">pirveli message</h1>
-            </div>
-            <div className=" mt-4 p-3 flex justify-end  ">
-              <h1 className="bg-red-600 p-3 rounded-3xl">Meore Message message</h1>
-            </div>
-          </div>
-        </div>
-        <div className="flex justify-center text-center items-center gap-3">
-          <h1>Logo</h1>
-          <input
-            type="Whats In Your Mind?"
-            className="bg-slate-700 p-3 w-[500px] outline-none rounded-3xl"
-          />
-          <h1>Logo</h1>
-        </div>
-      </div>
+        <Chat id={userId}/>
     </div>
   );
 }
